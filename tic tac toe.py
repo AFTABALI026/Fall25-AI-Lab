f85496f23935e356fc8e223f2b125779e5e916b1
AFTABALI026 <aftabali76888@gmail.com>
2025-10-15 12:16:28 +0500

    Update tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,7 +1,4 @@
 
-# Author: aqeelanwar
-# Created: 12 March,2020, 7:06 PM
-# Email: aqeel.anwar@gatech.edu
 
 from tkinter import *
 import numpy as np
@@ -213,3 +210,4 @@ class Tic_Tac_Toe():
 
 game_instance = Tic_Tac_Toe()
 game_instance.mainloop()
+
